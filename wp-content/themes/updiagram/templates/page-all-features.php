f5a5e9585a4e0c496d2cb357479247e2a03ad788
tommy.d <tommy.d@itcgroup.io>
2021-10-04 10:59:58 +0700

    dd

--- wp-content/themes/updiagram/templates/page-all-features.php
+++ wp-content/themes/updiagram/templates/page-all-features.php
@@ -4,9 +4,10 @@
     Template Name: Page All Features
 */
 
-include get_template_directory()."/includes/header.php";
+phpinfo();
+/* include get_template_directory()."/includes/header.php";
 ?>
 
 <h1>Content of All Features page</h1>
 
-<?php include get_template_directory()."/includes/footer.php"; ?>
+<?php include get_template_directory()."/includes/footer.php"; ?> */
